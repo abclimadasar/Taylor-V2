d247b8e5ae5caa75c8399178db778c2804c1fb6e
AyGemuy <91945119+AyGemuy@users.noreply.github.com>
2023-11-08 16:12:55 +0800

    Add files via upload

--- plugins/Downloader/downloader-soundcloud.js
+++ plugins/Downloader/downloader-soundcloud.js
@@ -1,33 +1,43 @@
-const CLIENT_ID = 'zZeR6I5DM5NMAYEhk7J9vveMqZzpCIym'
-import soundcloud from 'soundcloud-downloader'
-import util from 'util'
-import fetch from 'node-fetch'
+const CLIENT_ID = 'zZeR6I5DM5NMAYEhk7J9vveMqZzpCIym';
+import soundcloud from 'soundcloud-downloader';
+import fetch from 'node-fetch';
+import util from 'util';
 
-let handler = async (m, { conn, args, usedPrefix, command }) => {
+const handler = async (m, { conn, args, usedPrefix, command }) => {
+  try {
+    const who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : (m.fromMe ? conn.user.jid : m.sender);
+    const pp = await conn.getProfilePicture(who);
+    const name = await conn.getName(who);
 
-let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-let pp = await conn.profilePictureUrl(who).catch(_ => hwaifu.getRandom())
-let name = await conn.getName(who)
-    if (!args[0]) throw `Use example ${usedPrefix}${command} link`
-await soundcloud.download(args[0], CLIENT_ID).then(async (buff) => {
-            let hasil = await fetch(buff)
-        await conn.sendFile(m.chat, hasil, command + '.mp3', '', m, null, { fileLength: fsizedoc, seconds: fsizedoc, contextInfo: {
-            mimetype: 'audio/mp4',
-          externalAdReply :{
-    mediaUrl: sig,
-    mediaType: 2,
-    description: wm, 
-    title: '👋 Hai, ' + name + ' ' + ucapan,
-    body: botdate,
-    thumbnail: await(await fetch(pp)).buffer(),
-    sourceUrl: hasil
-     }}
-  })
-  }
+    if (!args[0]) {
+      throw new Error(`Gunakan contoh ${usedPrefix}${command} link`);
     }
-handler.help = ['soundcloud'].map(v => v + ' <url>')
-handler.tags = ['downloader']
 
-handler.command = /^s(oundcloud(d(own|l)2|2)|cd(own|l)2)$/i
+    const buff = await soundcloud.download(args[0], CLIENT_ID);
+    const hasil = await fetch(buff);
 
-export default handler
+    await conn.sendFile(m.chat, hasil.body, `${command}.mp3`, '', m, null, {
+      mimetype: 'audio/mpeg',
+      seconds: 120, // Gantilah sesuai durasi lagu yang sesuai
+      contextInfo: {
+        externalAdReply: {
+          mediaUrl: hasil.url,
+          mediaType: 2,
+          description: 'Deskripsi Anda',
+          title: `👋 Hai, ${name} ucapan`,
+          body: 'Botdate Anda',
+          thumbnail: await fetch(pp).then(res => res.buffer()),
+          sourceUrl: hasil.url,
+        },
+      },
+    });
+  } catch (error) {
+    throw new Error(error.message); // Tangkap dan lempar error yang sesuai
+  }
+};
+
+handler.help = ['soundcloud'].map(v => `${v} <url>`);
+handler.tags = ['downloader'];
+handler.command = /^s(oundcloud(d(own|l)2|2)|cd(own|l)2)$/i;
+
+export default handler;
