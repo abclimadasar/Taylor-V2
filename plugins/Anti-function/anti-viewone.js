df34037ff9c3a3f2729cb63eaa6e3b66a61b12cb
AyGemuy <91945119+AyGemuy@users.noreply.github.com>
2023-11-08 16:12:11 +0800

    Add files via upload

--- plugins/Anti-function/anti-viewone.js
+++ plugins/Anti-function/anti-viewone.js
@@ -1,28 +1,30 @@
-let { downloadContentFromMessage } = (await import('@adiwajshing/baileys'));
-import fetch from 'node-fetch'
+import { downloadContentFromMessage } from '@adiwajshing/baileys';
+import fetch from 'node-fetch';
 
 export async function before(m) {
-let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? this.user.jid : m.sender
-let pp = await this.profilePictureUrl(who).catch(_ => hwaifu.getRandom())
-let name = await this.getName(who)
- 
-let chat = global.db.data.chats[m.chat]
-    if (!chat.viewonce) return
-    if (m.mtype == 'viewOnceMessage') {
-        let msg = m.message.viewOnceMessage.message
-        let type = Object.keys(msg)[0]
-        let media = await downloadContentFromMessage(msg[type], type == 'imageMessage' ? 'image' : 'video')
-        let buffer = Buffer.from([])
-        for await (const chunk of media) {
-            buffer = Buffer.concat([buffer, chunk])
-        }
-        if (/video/.test(type)) {
-            return this.sendFile(m.chat, buffer, author, msg[type].caption || '', m, null, fakefb)
-            return throw '[View Once Video] Detected'
-            
-        } else if (/image/.test(type)) {
-            return this.sendFile(m.chat, buffer, author, msg[type].caption || '', m, null, fakefb)
-            return throw '[View Once Image] Detected'
-        }
+  const who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : (m.fromMe ? this.user.jid : m.sender);
+  const pp = await this.profilePictureUrl(who).catch(_ => hwaifu.getRandom());
+  const name = await this.getName(who);
+
+  const chat = global.db.data.chats[m.chat];
+  if (!chat.viewonce) return;
+
+  if (m.mtype == 'viewOnceMessage') {
+    const msg = m.message.viewOnceMessage.message;
+    const type = Object.keys(msg)[0];
+    const media = await downloadContentFromMessage(msg[type], type == 'imageMessage' ? 'image' : 'video');
+    let buffer = Buffer.from([]);
+
+    for await (const chunk of media) {
+      buffer = Buffer.concat([buffer, chunk]);
     }
+
+    if (/video/.test(type)) {
+      this.sendFile(m.chat, buffer, author, msg[type].caption || '', m, null, fakefb);
+      throw new Error('[View Once Video] Detected');
+    } else if (/image/.test(type)) {
+      this.sendFile(m.chat, buffer, author, msg[type].caption || '', m, null, fakefb);
+      throw new Error('[View Once Image] Detected');
+    }
+  }
 }
