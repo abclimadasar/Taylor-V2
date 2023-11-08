7c2d68d4fc389da77c848f16ffada4bfef04bb80
AyGemuy <91945119+AyGemuy@users.noreply.github.com>
2023-11-08 16:13:43 +0800

    Add files via upload

--- plugins/Answer-function/menfess-ans.js
+++ plugins/Answer-function/menfess-ans.js
@@ -1,42 +1,35 @@
-import fs from 'fs'
-const delay = time => new Promise(res => setTimeout(res, time))
+import fs from 'fs';
+const delay = (time) => new Promise((res) => setTimeout(res, time));
+
 export async function before(m) {
-	if (!m.chat.endsWith('@s.whatsapp.net')) return !0;
-	this.menfess = this.menfess ? this.menfess : {}
-	let mf = Object.values(this.menfess).find(v => v.status === false && v.penerima == m.sender)
-	if (!mf) return !0
-	console.log({ text: m.text, type: m.quoted?.mtype })
-	if ((m.text === 'BALAS PESAN' || m.text === '') && m.quoted.mtype == 'buttonsMessage') return m.reply("Silahkan kirim pesan balasan kamu.\nKetik pesan sesuatu lalu kirim, maka pesan otomatis masuk ke target balas pesan.");
-	else {
-		let imgr = fla.getRandom()
-		let txt = `Hᴀɪ ᴋᴀᴋ @${mf.dari.split('@')[0]}, ᴋᴀᴍᴜ ᴍᴇɴᴇʀɪᴍᴀʜ ʙᴀʟᴇsᴀɴ ɴɪʜ.Pesan yang kamu kirim sebelumnya:\n${mf.pesan}\n\nPesan balasannya:\n${m.text}\n`.trim();
-		let sblm = `Pᴇsᴀɴ Aɴᴅᴀ Sᴇʙᴇʟᴜᴍɴʏᴀ ➛ ${mf.pesan}\nPᴇsᴀɴ Bᴀʟᴀsᴀɴɴʏᴀ ➨ ${m.text}`
-		await this..reply(mf.dari, txt+'\n'+sblm,fakes, { contextInfo: { externalAdReply: { showAdAttribution: true,
-    mediaUrl: sig,
-    mediaType: 2, 
-    description: sgc,
-    title: "Jᴏɪɴ Sɪɴɪ Cᴜʏ",
-    body: wm,
-    thumbnail: fs.readFileSync('./thumbnail.jpg'),
-    sourceUrl: sgc}}})
-		/*await this.sendButton(mf.dari, bottime, txt, `${imgr + 'Menfess'}`, [['BALAS PESAN', '.balasmenfess']], null)*/.then(() => {
-			m.reply('Berhasil Mengirim balasan.')
-			delay(1500)
-			delete this.menfess[mf.id]
-			return !0
-		})
-	}
-	return !0
+  if (!m.chat.endsWith('@s.whatsapp.net')) return true;
+
+  this.menfess = this.menfess ? this.menfess : {};
+  const mf = Object.values(this.menfess).find((v) => v.status === false && v.penerima == m.sender);
+
+  if (!mf) return true;
+
+  console.log({ text: m.text, type: m.quoted?.mtype });
+
+  if ((m.text === 'BALAS PESAN' || m.text === '') && m.quoted.mtype == 'buttonsMessage') {
+    await conn.reply(m.chat, "Silahkan kirim pesan balasan kamu.\nKetik pesan sesuatu lalu kirim, maka pesan otomatis masuk ke target balas pesan", null)
+    .then(() => {
+      m.reply("Silahkan kirim pesan balasan kamu.\nKetik pesan sesuatu lalu kirim, maka pesan otomatis masuk ke target balas pesan.");
+    });
+  } else {
+    const imgr = fla.getRandom();
+    const txt = `Hᴀɪ ᴋᴀᴋ @${mf.dari.split('@')[0]}, ᴋᴀᴍᴜ ᴍᴇɴᴇʀɪᴛᴇ ʙᴀʟᴀsᴀɴ ɴɪʜ. Pesan yang kamu kirim sebelumnya:\n${mf.pesan}\n\nPesan balasannya:\n${m.text}\n`.trim();
+    const sblm = `Pᴇsᴀɴ Aɴᴅᴀ Sᴇʙᴇʟᴜᴍɴʏᴀ ➛ ${mf.pesan}\nPᴇsᴀɴ Bᴀʟᴀsᴀɴɴʏᴀ ➨ ${m.text}`;
+
+    await conn.reply(m.chat, txt + '\n' + sblm, null)
+    .then(() => {
+      conn.reply(m.chat, 'Berhasil Mengirim balasan.', null)
+      .then(() => {
+        delay(1500);
+        delete this.menfess[mf.id];
+      });
+    });
+  }
+
+  return true;
 }
-/* Made By FokusDotId (Fokus ID)
- * https://github.com/FokusDotId
- * Ingin bikin fitur tapi tidak bisa coding?
- * hubungi: https://wa.me/6281320170984
- * Jangan lupa bawa udut minimal sukun🗿
- 
- *Nᴜᴍᴘᴀɴɢ Nᴀᴍᴀ*
- *Kᴀɴɢ Rᴇᴄᴏᴅᴇ : Aʟᴅɪ Lᴇsᴍᴀɴᴀ
- *Cʜᴀɴɴᴇʟ : AL? Offc
- 
- *Minimal Jangan Di Hapos Anj
-*/
