581ee33a05e526a934afe40bdf7f84d6422061fd
AyGemuy <91945119+AyGemuy@users.noreply.github.com>
2023-11-10 22:02:57 +0800

    Add files via upload

--- plugins/AI/ai-zcode.js
+++ plugins/AI/ai-zcode.js
@@ -5,29 +5,117 @@ let handler = async (m, {
 	command,
 	usedPrefix
 }) => {
-	const [model, question] = text.split("|")
-	if (!(model, question)) return m.reply("*Example:*\n" + usedPrefix + command + " js|buat console log")
-	try {
-		const msg = await fetchAndParse(model, question)
-		await conn.reply(m.chat, msg, m, adReply)
-	} catch (e) {
-		return m.reply(eror)
+	if (!(m.quoted && m.quoted.text && text)) return m.reply("*Example:*\n" + usedPrefix + command + " js (with reply code)")
+	const model = text.trim();
+	const question = m.quoted.text;
+
+	if (command === "zcodequest") {
+		try {
+			const payload = {
+				p1: model,
+				p2: question,
+				option1: "3 - A detailed answer",
+				option2: "Professional",
+				option3: "Indonesian"
+			};
+			const msg = await fetchAndParse(payload, 'answer-question')
+			await m.reply(msg)
+		} catch (e) {
+			return m.reply(eror)
+		}
 	}
+
+	if (command === "zcodegen") {
+		try {
+			const payload = {
+				p1: model,
+				p2: question,
+				option1: "3 - A detailed answer",
+				option2: "Professional",
+				option3: "Indonesian"
+			};
+			const msg = await fetchAndParse(payload, 'code-generator')
+			await m.reply(msg)
+		} catch (e) {
+			return m.reply(eror)
+		}
+	}
+
+	if (command === "zcodebug") {
+		try {
+			const payload = {
+				p1: model,
+				p2: null,
+				p3: question,
+				option1: "find and explain bug",
+				option2: "Professional",
+				option3: "Indonesian"
+			};
+			const msg = await fetchAndParse(payload, 'code-debug')
+			await m.reply(msg)
+		} catch (e) {
+			return m.reply(eror)
+		}
+	}
+
+	if (command === "zcoderef") {
+		try {
+			const payload = {
+				p1: model,
+				p2: null,
+				p3: question,
+				option1: "Refactor my code and explain me",
+				option2: "Professional",
+				option3: "Indonesian"
+			};
+			const msg = await fetchAndParse(payload, 'code-refactor')
+			await m.reply(msg)
+		} catch (e) {
+			return m.reply(eror)
+		}
+	}
+
+	if (command === "zcoderev") {
+		try {
+			const payload = {
+				p1: model,
+				p2: null,
+				p3: question,
+				option1: "Make a full code review",
+				option2: "Professional",
+				option3: "Indonesian"
+			};
+			const msg = await fetchAndParse(payload, 'code-review')
+			await m.reply(msg)
+		} catch (e) {
+			return m.reply(eror)
+		}
+	}
+
+	if (command === "zcodedoc") {
+		try {
+			const payload = {
+				p1: model,
+				p2: null,
+				p3: question,
+				option1: "Add comment everwhere you can",
+				option2: "Professional",
+				option3: "Indonesian"
+			};
+			const msg = await fetchAndParse(payload, 'code-documentation')
+			await m.reply(msg)
+		} catch (e) {
+			return m.reply(eror)
+		}
+	}
+
 }
-handler.command = /^(zcode)$/i
+handler.command = /^(zcode(quest|gen|bug|ref|rev|doc))$/i
 export default handler
 
-async function fetchAndParse(p1, p2) {
+async function fetchAndParse(payload, tool) {
 	try {
-		const url = 'https://zzzcode.ai/api/tools/answer-question';
-		const payload = {
-			p1,
-			p2,
-			option1: "3 - A detailed answer",
-			option2: "Professional",
-			option3: "Indonesian"
-		};
-
+		const url = 'https://zzzcode.ai/api/tools/' + tool;
 		const response = await fetch(url, {
 			method: 'POST',
 			headers: {
@@ -39,7 +127,7 @@ async function fetchAndParse(p1, p2) {
 		const match = input.match(/zzzredirectmessageidzzz:\s*([a-zA-Z0-9-]+)/);
 		const id = match ? match[1] : null;
 
-		const url2 = 'https://zzzcode.ai/api/tools/answer-question';
+		const url2 = 'https://zzzcode.ai/api/tools/' + tool;
 		const response2 = await fetch(url2, {
 			method: 'POST',
 			headers: {
