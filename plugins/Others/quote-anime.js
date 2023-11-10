f60908b31b1b5f204cb3bf0b471b3c63887e8aad
AyGemuy <91945119+AyGemuy@users.noreply.github.com>
2023-11-10 22:00:58 +0800

    Add files via upload

--- plugins/Others/quote-anime.js
+++ plugins/Others/quote-anime.js
@@ -1,13 +1,37 @@
-import fetch from 'node-fetch'
-let handler = async(m, { conn, text }) => {
-  let res = await (await fetch('https://katanime.vercel.app/api/getrandom?limit=1'))
-  if (!res.ok) throw await res.text()
-  let json = await res.json()
-  if(!json.result[0]) throw json
-  let { indo, character, anime } = json.result[0]
-  conn.reply(m.chat, `Quotes:\n${indo}\n\n~ Character:\n${character}\n~Anime:\n${anime}`, m)
+import fetch from 'node-fetch';
+
+const handler = async (m, { conn, text }) => {
+  try {
+    const { quote, character, anime } = text ? await quotesByName(text) : await quotesRandom();
+    conn.reply(m.chat, `Quotes:\n${quote}\n\n~ Character:\n${character}\n~Anime:\n${anime}`, m);
+  } catch (error) {
+    console.error("Error in quotesanime handler:", error);
+    conn.reply(m.chat, "Terjadi kesalahan dalam mengeksekusi perintah.", m);
+  }
+};
+
+handler.help = ['quotesanime'];
+handler.tags = ['anime', 'quotes'];
+handler.command = /^(quotesanime|kataanime)$/i;
+
+async function quotesRandom() {
+  try {
+    const { quote, character, anime } = await (await fetch("https://animechan.xyz/api/random")).json();
+    return { quote, character, anime };
+  } catch (error) {
+    console.error("Error fetching random quote:", error);
+    return null;
+  }
 }
-handler.help = ['quotesanime']
-handler.tags = ['anime', 'quotes']
-handler.command = /^(quotesanime|kataanime)$/i
-export default handler 
+
+async function quotesByName(name) {
+  try {
+    const { quote, character, anime } = await (await fetch(`https://animechan.xyz/api/random/character?name=${name}`)).json();
+    return { quote, character, anime };
+  } catch (error) {
+    console.error(`Error fetching ${name}'s quote:`, error);
+    return null;
+  }
+}
+
+export default handler;
