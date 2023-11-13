b41ea53f89fb835352d446cf1ef8bd0a26fd2adc
AyGemuy <91945119+AyGemuy@users.noreply.github.com>
2023-11-13 11:38:33 +0800

    Add files via upload

---
+++ plugins/GPT/gpt-gptphoto.js
@@ -0,0 +1,60 @@
+import fetch from "node-fetch"
+
+let handler = async (m, {
+    conn,
+    args,
+    usedPrefix,
+    command
+}) => {
+    let text
+    if (args.length >= 1) {
+        text = args.slice(0).join(" ")
+    } else if (m.quoted && m.quoted.text) {
+        text = m.quoted.text
+    } else throw "Input Teks"
+    await m.reply(wait)
+    
+    try {
+        let data = await generateImage(text)
+        if (data && data.imgs.length > 0) {
+                for (let i = 0; i < data.imgs.length; i++) {
+                await conn.sendFile(m.chat, data.imgs[i], '', `Image *(${i + 1}/${data.imgs.length})*`, m, false, {
+                    mentions: [m.sender]
+                });
+            }
+            }
+    } catch (e) {
+        await m.reply(eror)
+    }
+}
+handler.help = ["gptphoto"]
+handler.tags = ["gpt"];
+handler.command = /^(gptphoto)$/i
+
+export default handler
+
+/* New Line */
+async function generateImage(captionInput) {
+  const data = {
+    captionInput,
+    captionModel: "default"
+  };
+
+  const url = 'https://chat-gpt.photos/api/generateImage';
+
+  try {
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    });
+
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.error("Error:", error);
+    throw error;
+  }
+}
