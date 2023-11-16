439cfb6f3f3d6e8a07d9c8cc4dbde57260a9ba85
AyGemuy <91945119+AyGemuy@users.noreply.github.com>
2023-11-16 10:39:24 +0800

    Add files via upload

---
+++ plugins/GPT/gpt-aichatonline.js
@@ -0,0 +1,51 @@
+import axios from 'axios';
+
+let handler = async (m, {
+    conn,
+    args,
+    usedPrefix,
+    command
+}) => {
+    let text
+    if (args.length >= 1) {
+        text = args.slice(0).join(" ")
+    } else if (m.quoted && m.quoted.text) {
+        text = m.quoted.text
+    } else throw "Input Teks"
+    await m.reply(wait)
+try {
+let result = await generate(text)
+await m.reply(result.reply)
+} catch (e) {
+await m.reply(eror)
+}
+}
+handler.help = ["aichatonline"]
+handler.tags = ["gpt"];
+handler.command = /^(aichatonline)$/i
+export default handler
+
+/* New Line */
+async function generate(q) {
+  try {
+    const { data } = await axios(
+      `https://aichatonline.org/wp-json/mwai-ui/v1/chats/submit`,
+      {
+        method: "post",
+        data: {
+          botId: "default",
+          newMessage: q,
+          stream: false,
+        },
+        headers: {
+          Accept: "text/event-stream",
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    return data;
+  } catch (err) {
+    console.log(err.response.data);
+    return err.response.data.message;
+  }
+}
