a7146614a66cc8829b348a8ea0ad7bfff4e7c03c
AyGemuy <91945119+AyGemuy@users.noreply.github.com>
2023-11-20 02:22:32 +0800

    Add files via upload

--- plugins/AI/ai-chat.js
+++ plugins/AI/ai-chat.js
@@ -1,21 +1,28 @@
-import Aichat from '../../lib/ai/aichat.js';
-const model = 'gpt-3.5-turbo';
+import Aichat from "../../lib/ai/aichat.js";
+const model = "gpt-3.5-turbo";
 
-const handler = async (m, { text }) => {
-  if (!text) throw 'Contoh: .aichat Pesan yang ingin Anda sampaikan kepada asisten AI';
+let handler = async (m, {
+    conn,
+    args,
+    usedPrefix,
+    command
+}) => {
+    let text
+    if (args.length >= 1) {
+        text = args.slice(0).join(" ")
+    } else if (m.quoted && m.quoted.text) {
+        text = m.quoted.text
+    } else throw "Input Teks"
+    await m.reply(wait)
+    try {
+        const messages = [{ role: "system", content: "Anda adalah asisten yang membantu." }, { role: "user", content: text }];
 
-  m.reply(wait);
-  const messages = [{ role: 'system', content: 'Anda adalah asisten yang membantu.' }, { role: 'user', content: encodeURIComponent(text) }];
-
-  try {
     const output = await Aichat.createAsync(model, messages);
-    m.reply(output);
-  } catch (error) {
-    console.error('Error:', error);
-    m.reply(eror);
-  }
-};
-
+    await m.reply(output);
+    } catch (e) {
+        await m.reply(eror)
+    }
+}
 handler.help = ['aichat'];
 handler.tags = ['ai'];
 handler.command = /^(aichat)$/i;
